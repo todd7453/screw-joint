f5b3b8959c2b5fcaf0735125322c088ef55a00ea
LoCa <LoCa>
2024-11-11 15:25:23 +0800

    setup Heroku

--- screw_joint/settings.py
+++ screw_joint/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'screw_joint.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3',conn_max_age=600, ssl_require=True)
 }
 
 # Password validation
